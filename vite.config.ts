2a08fb656a3abd824f1cce3085d0c752dd7dae36
Максим Мольков <maksim994@yandex.ru>
2025-08-31 17:10:54 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,16 +1,17 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { ProxyServer } from 'http-proxy'; // Импорт типа для прокси
 
 export default defineConfig({
   server: {
-    allowedHosts: ['b6da5e64b6a0.ngrok-free.app'], // Твой ngrok-URL
+    allowedHosts: ['b203ebf4d2d5.ngrok-free.app'], // Твой ngrok-URL
     proxy: {
       '/api/list': {
         target: 'https://g-qr.ru',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api\/list/, '/api/1/list'),
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', (proxyReq, req) => {
+        configure: (proxy: ProxyServer, _options) => {
+          proxy.on('proxyReq', (proxyReq: any, req: any) => {
             console.log('Proxy request URL for /api/list:', req.url); // Отладка
           });
         },
@@ -19,8 +20,8 @@ export default defineConfig({
         target: 'https://g-qr.ru',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '/api/1/validate'),
-        configure: (proxy, _options) => {
-          proxy.on('proxyReq', (proxyReq, req) => {
+        configure: (proxy: ProxyServer, _options) => {
+          proxy.on('proxyReq', (proxyReq: any, req: any) => {
             console.log('Proxy request URL for /api:', req.url); // Отладка
           });
         },
